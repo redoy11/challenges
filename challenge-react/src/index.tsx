175baca65702876dd1a1f5a99079597bdda34ec8
redoy11 <redoyahmed11@gmail.com>
2021-05-18 00:54:06 +0600

    Modify index to use the new store

--- challenge-react/src/index.tsx
+++ challenge-react/src/index.tsx
@@ -1,34 +1,10 @@
 import React from 'react';
-import { render } from 'react-dom';
-import { createStore } from 'redux';
+import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
-import App from './App';
+import App from './app/App';
+import store from './store';
 
-const store = createStore(function (state, action: any) {
-  const _state: any =
-    state == null
-      ? {
-          donate: 0,
-          message: '',
-        }
-      : state;
-
-  switch (action.type) {
-    case 'UPDATE_TOTAL_DONATE':
-      return Object.assign({}, _state, {
-        donate: _state.donate + action.amount,
-      });
-    case 'UPDATE_MESSAGE':
-      return Object.assign({}, _state, {
-        message: action.message,
-      });
-
-    default:
-      return _state;
-  }
-});
-
-render(
+ReactDom.render(
   <Provider store={store}>
     <App />
   </Provider>,
