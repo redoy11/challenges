aba4f693053166be66accfe8827f37efaffb118c
redoy11 <redoyahmed11@gmail.com>
2021-05-18 23:20:13 +0600

    Add content type header on post request

--- challenge-react/src/utils/requests.ts
+++ challenge-react/src/utils/requests.ts
@@ -32,6 +32,9 @@ const postRequest = async (
 ): Promise<unknown> => {
   const response = await fetch(endpoint, {
     method: POST,
+    headers: {
+      'Content-Type': 'application/json',
+    },
     body: JSON.stringify(body),
   });
   return response;
