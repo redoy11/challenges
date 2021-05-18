208ba97b4e5209c7b642efb75a85f2ed6cdd4bc2
redoy11 <redoyahmed11@gmail.com>
2021-05-18 22:49:29 +0600

    Add constants file

---
+++ challenge-react/src/configs/constants.ts
@@ -0,0 +1,7 @@
+/** global constants */
+
+/** the initial selected donation amount */
+export const INITIAL_SELECTED_DONATION_AMOUNT = 10;
+
+/** the default amount list */
+export const DEFAULT_DONATION_AMOUNTS = [10, 20, 50, 100, 500];
