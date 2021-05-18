cce87e462016e2ba7b720bb612cd56db48385210
redoy11 <redoyahmed11@gmail.com>
2021-05-18 22:49:14 +0600

    Add endpoints file

---
+++ challenge-react/src/configs/endpoints.ts
@@ -0,0 +1,10 @@
+/** server endpoints */
+
+/** default server endpoint */
+export const SERVER_ENDPOINT = 'http://localhost:3001';
+
+/** server charities endpoint */
+export const SERVER_CHARITIES_ENDPOINT = `${SERVER_ENDPOINT}/charities`;
+
+/** server payments endpoint */
+export const SERVER_PAYMENTS_ENDPOINT = `${SERVER_ENDPOINT}/payments`;
