c461f5a58d2b8c90dccf76b4cf5be3a35a248fd5
redoy11 <redoyahmed11@gmail.com>
2021-05-20 16:07:07 +0600

    Add a docstring to summaryDonations method

--- challenge-react/src/utils/helpers.ts
+++ challenge-react/src/utils/helpers.ts
@@ -1,2 +1,7 @@
-export const summaryDonations = (danations) =>
-  danations.reduce((accumulator, value) => accumulator + value);
+/**
+ * calculates the sum of donations
+ * @param {number[]} donations - the array containing the exisiting donation amounts
+ * @returns {number} - the sum of donations
+ */
+export const summaryDonations = (donations: number[]): number =>
+  donations.reduce((accumulator, value) => accumulator + value);
