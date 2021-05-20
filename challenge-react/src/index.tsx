0a4dbc5f465e636beb59f29d2d33d3ab6238b764
redoy11 <redoyahmed11@gmail.com>
2021-05-20 15:45:01 +0600

    Add a global style file

--- challenge-react/src/index.tsx
+++ challenge-react/src/index.tsx
@@ -3,9 +3,11 @@ import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './app/App';
 import store from './store';
+import GlobalStyle from './globalStyle';
 
 ReactDom.render(
   <Provider store={store}>
+    <GlobalStyle />
     <App />
   </Provider>,
   document.getElementById('root')
