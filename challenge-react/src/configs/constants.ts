f779872dd13f6b35383e152c82b09ec678467d04
redoy11 <redoyahmed11@gmail.com>
2021-05-20 22:57:51 +0600

    Add notification related constants

--- challenge-react/src/configs/constants.ts
+++ challenge-react/src/configs/constants.ts
@@ -5,3 +5,23 @@ export const INITIAL_SELECTED_DONATION_AMOUNT = 10;
 
 /** the default amount list */
 export const DEFAULT_DONATION_AMOUNTS = [10, 20, 50, 100, 500];
+
+/** notifications */
+
+/** the default notification color */
+export const DEFAULT_COLOR = 'gray';
+
+/** the success notification color */
+export const SUCCESS_COLOR = 'green';
+
+/** the error notification color */
+export const ERROR_COLOR = 'red';
+
+/** notification messages */
+
+/** success message */
+export const SUCCESS_MESSAGE =
+  'Donation Successful. Thank you for your support';
+
+/** error message */
+export const ERROR_MESSAGE = 'Something went wrong. Please Try again later';
